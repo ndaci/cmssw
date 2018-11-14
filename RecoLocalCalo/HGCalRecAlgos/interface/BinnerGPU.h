0b8a77e11bd5be35cbe71e2ec9d6cde9159b97be
tonydp03 <tony.dipilato03@gmail.com>
2018-11-14 12:06:41 +0100

    Introducing Histogram class and the new kernel

---
+++ RecoLocalCalo/HGCalRecAlgos/interface/BinnerGPU.h
@@ -0,0 +1,44 @@
+#include <memory>
+#include <iostream>
+#include <vector>
+#include "RecoLocalCalo/HGCalRecAlgos/interface/GPUHist2D.h"
+
+#ifndef Binner_GPU_h
+#define Binner_GPU_h
+
+struct RechitForBinning {
+        unsigned int index;
+        float eta;
+        float phi;
+};
+
+
+struct RechitForBinning2 {
+        unsigned int index;
+};
+
+typedef std::vector<std::vector<RechitForBinning>> BinningData;
+typedef std::vector<RechitForBinning> LayerData;
+
+typedef std::vector<std::vector<RechitForBinning2>> BinningData2;
+
+
+namespace BinnerGPU {
+    // eta_width = 0.05
+    // phi_width = 0.05
+    // 2*pi/0.05 = 125
+    // 1.4/0.05 = 28
+    // 20 (as heuristic)
+
+const int ETA_BINS=28;
+const int PHI_BINS=126;
+const int MAX_DEPTH=20;
+
+
+
+ histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH> computeBins(LayerData layerData);
+
+}
+
+
+#endif
---
+++ RecoLocalCalo/HGCalRecAlgos/src/BinnerGPU.cu
@@ -0,0 +1,61 @@
+#include "RecoLocalCalo/HGCalRecAlgos/interface/BinnerGPU.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/GPUVecArray.h"
+#include <math.h>
+
+#include "RecoLocalCalo/HGCalRecAlgos/interface/GPUHist2D.h"
+
+
+namespace BinnerGPU {
+
+
+  __global__ void kernel_compute_histogram(RechitForBinning*dInputData, histogram2D<int,ETA_BINS, PHI_BINS, MAX_DEPTH> *dOutputData, const size_t numRechits) {
+
+    size_t rechitLocation = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(rechitLocation >= numRechits)
+        return;
+
+    float eta = dInputData[rechitLocation].eta;
+    float phi = dInputData[rechitLocation].phi;
+    unsigned int index = dInputData[rechitLocation].index;
+   
+    dOutputData->fillBinGPU(eta, phi, index);
+
+  }
+
+
+  float minEta = 1.6;
+  float maxEta = 3.0;
+  float minPhi = -M_PI;
+  float maxPhi = M_PI;
+
+  histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH> computeBins(std::vector<RechitForBinning> layerData) {
+
+    histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH> hOutputData(minEta, maxEta, minPhi, maxPhi);
+
+    // Allocate memory and put data into device
+    histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH> *dOutputData;
+    RechitForBinning* dInputData;
+    cudaMalloc(&dOutputData, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>));
+    cudaMalloc(&dInputData, sizeof(RechitForBinning)*layerData.size());
+    cudaMemcpy(dInputData, layerData.data(), sizeof(RechitForBinning)*layerData.size(), cudaMemcpyHostToDevice);
+    cudaMemset(dOutputData, 0x00, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>));
+    cudaMemcpy(dOutputData, &hOutputData, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>), cudaMemcpyHostToDevice);
+  
+    // Call the kernel
+    const dim3 blockSize(1024,1,1);
+    const dim3 gridSize(ceil(layerData.size()/1024.0),1,1);
+    kernel_compute_histogram <<<gridSize,blockSize>>>(dInputData, dOutputData, layerData.size());
+
+    // Copy result back!
+    cudaMemcpy(dOutputData, &hOutputData, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>), cudaMemcpyDeviceToHost);
+
+    // Free all the memory
+    cudaFree(dOutputData);
+    cudaFree(dInputData);
+
+    
+    return hOutputData;
+  }
+
+}
