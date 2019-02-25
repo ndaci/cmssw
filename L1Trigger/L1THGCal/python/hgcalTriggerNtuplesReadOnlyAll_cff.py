bd143e7d66681fbcfe63fbed6195ef41c15bcb07
Nadir Daci <daci.nadir@gmail.com>
2019-02-25 12:12:01 +0100

    Adding read-only ntuplizers.

---
+++ L1Trigger/L1THGCal/python/hgcalTriggerNtuplesReadOnlyAll_cff.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+from L1Trigger.L1THGCal.hgcalTriggerNtuplesReadOnlyAll_cfi import *
+
+hgcalTriggerNtuplesReadOnlyAll = cms.Sequence(hgcalTriggerNtuplizerReadOnlyAll)
+
+
+
